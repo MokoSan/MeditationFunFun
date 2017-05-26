5853aa5067affb7a89392c5162026ddc61a7e95d
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-05-26 15:21:37 -0400

    Added the create db and user table script

---
+++ MeditationFunFun/SQL Scripts/create.sql
@@ -0,0 +1,14 @@
+﻿IF NOT EXISTS ( SELECT name from master.sys.databases WHERE name = N'MeditationFunFun' )
+BEGIN
+CREATE DATABASE MeditationFunFun
+END
+
+GO
+USE MeditationFunFun
+GO 
+CREATE TABLE Users (
+	Id int NOT NULL, 
+	Name nvarchar(120) NULL,
+	Email nvarchar(200) NULL
+	Primary Key (Id)
+)

89eeef98311ddb98880955e2a84d5e55beef5ec8
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-05-26 12:54:24 -0400

    Add project files.

---
+++ MeditationFunFun/Api.Common.fs
@@ -0,0 +1,11 @@
+﻿module Api.Common 
+
+    type RestResource<'TController> = {
+        GetAll      : unit                -> 'TController seq
+        GetById     : int                 -> 'TController option 
+        IsExists    : int                 -> bool
+        Create      : 'TController        -> 'TController
+        Update      : 'TController        -> 'TController option 
+        UpdateById  : int -> 'TController -> 'TController option
+        Delete      : int                 -> unit
+    }

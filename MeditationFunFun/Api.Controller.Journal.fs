776b7f2b519094d3de9579a956862be6600dfc24
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-05-26 15:14:39 -0400

    Added the Journal record type

---
+++ MeditationFunFun/Api.Controller.Journal.fs
@@ -0,0 +1,14 @@
+﻿module MeditationFunFun.Api.Controller.Journal 
+
+    open MeditationFunFun.Api.Common
+
+    open System
+
+    type Journal = {
+        Id          : int
+        UserId      : int 
+        Title       : string
+        CreatedTime : DateTime 
+        UpdatedTime : DateTime
+        Content     : string
+    }
